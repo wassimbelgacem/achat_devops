789b9104cfbdcdac0329fae971fae31332bd9ace
wassimbelgacem <92153159+wassimbelgacem@users.noreply.github.com>
2023-11-09 23:41:40 +0000

    ProduitTest.java

---
+++ src/test/java/tn/esprit/rh/achat/services/ProduitTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProduitTest {
+
+    @Test
+    void testProduitGetterAndSetter() {
+        Produit produit = new Produit();
+        produit.setCodeProduit("TestCode");
+        produit.setLibelleProduit("Test Libelle");
+        produit.setPrix(50.0f);
+
+        assertEquals("TestCode", produit.getCodeProduit());
+        assertEquals("Test Libelle", produit.getLibelleProduit());
+        assertEquals(50.0f, produit.getPrix(), 0.001); // Use delta for float comparisons
+    }
+
+    
+}
+
